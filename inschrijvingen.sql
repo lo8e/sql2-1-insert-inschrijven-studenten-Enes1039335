4e64f1b5e2550b5067ccc24c87df28d84deced25
Enes Can <47444407+Enes1039335@users.noreply.github.com>
2019-05-10 14:51:11 +0200

    Removed multiple USE

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,8 +1,5 @@
 USE `lo8e_sql2`;
+
 INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES ('lo9e-amo1', 'vlt03', 2019, 'amo');
-
-USE `lo8e_sql2`;
 INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES ('lo9e-amo2', 'rs001', 2019, 'amo');
-
-USE `lo8e_sql2`;
 INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES ('lo9e-amo3', 'brc01', 2019, 'amo');
