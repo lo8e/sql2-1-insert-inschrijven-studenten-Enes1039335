682b7f5c346490f30a8d9dd1946b101b766fafcb
Enes Can <47444407+Enes1039335@users.noreply.github.com>
2019-05-10 15:01:36 +0200

    added 4 students to studenten

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,14 @@
 USE `lo8e_sql2`;
-
+/*
 INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) 
 	VALUES 
 		('lo9e-amo1', 'vlt03', 2019, 'amo'),
 		('lo9e-amo2', 'rs001', 2019, 'amo'),
 		('lo9e-amo3', 'brc01', 2019, 'amo');
+*/
+INSERT INTO studenten (ov_nummer, achternaam, voornaam)
+	VALUES
+		(666666, 'Natte', 'Paddy'),
+        (666667, 'Can', 'Enes'),
+        (666668, 'Jobs', 'Steve'),
+        (666669, 'Gates', 'Bill');
